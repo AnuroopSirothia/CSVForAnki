45d1968c84e9f48fab69ddff1651e9b5788e5ffc
Anuroop <anuroop.sirothia@gmail.com>
2022-01-30 23:19:02 +0530

    File writing
    Basic functionality of writing the CSV formatted output in a file is up
    now. Though the format is not correct.

--- main.go
+++ main.go
@@ -9,6 +9,17 @@ import (
 )
 
 func main() {
+	// Open input file which contains notes.
+	notesFile := openNotesFile()
+
+	// Parse file
+	notesInCSVFormat := parseFile(notesFile)
+
+	// Save file
+	writeToCSVFile(notesInCSVFormat)
+}
+
+func openNotesFile() *os.File {
 	log.SetPrefix("csvforanki: ")
 	log.SetFlags(0)
 
@@ -23,7 +34,12 @@ func main() {
 		log.Fatal(e)
 	}
 
-	scanner := bufio.NewScanner(inputFile)
+	return inputFile
+}
+
+func parseFile(file *os.File) []byte {
+	scanner := bufio.NewScanner(file)
+	var parsedData []byte
 
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -33,20 +49,32 @@ func main() {
 		}
 
 		if line == "---" {
-			formatQA(scanner)
+			parsedData = append(parsedData, parseQA(scanner)...)
 		}
+	} //for end
+	return parsedData
+}
+
+func writeToCSVFile(data []byte) {
+	e := os.WriteFile("output/import_ready.txt", data, 0777)
+
+	if e != nil {
+		log.Fatal(e)
 	}
 }
 
-// Format the Question, Answer and Tag
-func formatQA(scanner *bufio.Scanner) {
-	var currentLine, currentLineButOne, currentLineButTwo string
-	var questionLine string
-	var answer strings.Builder
+// Parse the Question, Answer and Tag. Each tuple of question, answer and tag starts with --- and ends with ... like a YAML file.
+func parseQA(scanner *bufio.Scanner) []byte {
+	var currentLine, currentLineButOne, currentLineButTwo, questionLine string
+	var answerLines strings.Builder
+
+	var outputToSave []byte
 
 	scanner.Scan()
 	questionLine = scanner.Text()
-	fmt.Printf("\n%q,", questionLine)
+	questionOutput := fmt.Sprintf("\n%q,", questionLine)
+	fmt.Print(questionOutput)
+	outputToSave = append(outputToSave, []byte(questionOutput)...)
 
 	for scanner.Scan() {
 		currentLineButTwo = currentLineButOne
@@ -54,14 +82,21 @@ func formatQA(scanner *bufio.Scanner) {
 		currentLine = scanner.Text()
 
 		if currentLine == "..." {
-			answer.WriteString(currentLineButTwo)
-			fmt.Printf("%q,", answer.String())
-			fmt.Printf("%q,\n", currentLineButOne)
-			break
+			answerLines.WriteString(currentLineButTwo)
+
+			ansOutput := fmt.Sprintf("%q,", answerLines.String())
+			fmt.Print(ansOutput)
+			outputToSave = append(outputToSave, []byte(ansOutput)...)
+			tagOutput := fmt.Sprintf("%q,\n", currentLineButOne)
+			fmt.Print(tagOutput)
+			outputToSave = append(outputToSave, []byte(tagOutput)...)
+			return outputToSave
 		}
 
 		//		fmt.Println("Writing to answer buffer ", currentLineButTwo)
-		answer.WriteString(currentLineButTwo)
+		answerLines.WriteString(currentLineButTwo)
 
 	} //for end
+
+	return nil
 }
